ef7dafb6382fe34d22061fff108040759b3ddca4
Samuel_Roellin <samfromaway@gmail.com>
2022-11-07 09:32:47 +0100

    add

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,38 @@
 import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import { TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<'TabOne'>) {
+  const [text, onChangeText] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.title}>Tab Onee</Text>
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+      <Button
+        title="Add Todo"
+        onPress={() => setTodos((prev) => [text, ...prev])}
+        color="black"
+      />
+      {todos.map((e) => (
+        <View style={styles.list}>
+          <Text style={styles.listItem}>{e}</Text>
+        </View>
+      ))}
     </View>
   );
 }
@@ -19,6 +42,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
   title: {
     fontSize: 20,
@@ -28,5 +52,24 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: '100%',
+  },
+  list: {
+    padding: 6,
+    color: 'white',
+    width: '100%',
+  },
+  listItem: {
+    color: 'white',
+    backgroundColor: 'black',
+    width: '100%',
+    textAlign: 'center',
+    padding: 4,
   },
 });
